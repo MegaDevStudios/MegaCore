2556b11f6f398ee0c59acc336cb6a5ceb7dd9e9d
Dreaght <nikitaefser20092007@gmail.com>
2024-04-28 16:30:01 +0300

    initial commit

---
+++ src/main/java/dev/mega/megacore/MegaCore.java
@@ -0,0 +1,17 @@
+package dev.mega.megacore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MegaCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
