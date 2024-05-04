8cf21f6211eed6ad4a3536e55a2d8a71233800fa
dreaght <nikitaefser20092007@gmail.com>
2024-05-04 15:11:24 +0300

    Argument matcher test

---
+++ src/test/java/dev/mega/megacore/command/matcher/ArgumentMatcherTest.java
@@ -0,0 +1,12 @@
+package dev.mega.megacore.command.matcher;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArgumentMatcherTest {
+
+    @Test
+    void getValue() {
+    }
+}
