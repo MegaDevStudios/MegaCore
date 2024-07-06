cd2746078a68aa33a2b24b3ab359f5cdc1104858
Legeu21 <maksprostoved21@gmail.com>
2024-07-06 11:19:13 +0400

    rename AbstractMenu to Menu

--- src/main/java/dev/mega/megacore/listener/server/MenuListener.java
+++ src/main/java/dev/mega/megacore/listener/server/MenuListener.java
@@ -1,6 +1,6 @@
 package dev.mega.megacore.listener.server;
 
-import dev.mega.megacore.inventory.menu.AbstractMenu;
+import dev.mega.megacore.inventory.menu.Menu;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,8 +16,8 @@ public class MenuListener implements Listener {
         if (!(event.getWhoClicked() instanceof Player)) return;
 
         InventoryHolder holder = event.getInventory().getHolder();
-        if (!(holder instanceof AbstractMenu)) return;
-        AbstractMenu menu = (AbstractMenu) holder;
+        if (!(holder instanceof Menu)) return;
+        Menu menu = (Menu) holder;
 
         if (event.getCurrentItem() == null) return;
         if (event.getClickedInventory() != event.getView().getTopInventory())
